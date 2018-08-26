dd267fe632a09850cae466b82e06b62914369b3c
lypwonderful@163.com <lypwonderful@163.com>
2018-08-26 20:47:01 +0800

    add code of docker client

--- warehouse/contract/main.go
+++ warehouse/contract/main.go
@@ -1,44 +1,23 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"github.com/ctlog"
+	"github.com/iceforg/warehouse/contract/docker"
 )
 
+func ConfigInit() {
+	ctlog.SetLogLevel("info")
+	ctlog.SetLogDir("/tmp/log", "iceforg")
+}
+
 func main() {
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		panic(err)
-	}
+	ConfigInit()
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
+	// get docker client
+	docker.GetDockerClient()
+	ctlog.Infoln("================= STARTING ================")
+	ctlog.Debugf("================= STARTING ================%s", "asd")
 
-	for _, container := range containers {
-		fmt.Printf("%s %s %s\n", container.Names, container.ID[:10], container.Image)
-	}
+	//
 
-	cfg := types.ExecConfig{
-		Cmd: []string{"sleep", "10"},
-	}
-	ret, err := cli.ContainerExecCreate(context.Background(), "cli", cfg)
-	if err != nil {
-		fmt.Println("exec error:", err.Error())
-		return
-	}
-	fmt.Printf("msg:%+v\n", ret)
-
-	check := types.ExecStartCheck{
-		Tty: true,
-	}
-	err = cli.ContainerExecStart(context.Background(), ret.ID, check)
-	if err != nil {
-		fmt.Println("start error:", err.Error())
-		return
-	}
 }
